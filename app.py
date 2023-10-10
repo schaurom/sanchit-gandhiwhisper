9292fd685db114f9c27f459e701ced106694125b
schaurom <118014904+schaurom@users.noreply.github.com>
2023-10-10 23:27:06 +0200

    kleinstes Modell zum Testen

--- app.py
+++ app.py
@@ -8,7 +8,8 @@ from transformers.pipelines.audio_utils import ffmpeg_read
 import tempfile
 import os
 
-MODEL_NAME = "openai/whisper-large-v2"
+#MODEL_NAME = "openai/whisper-large-v2"
+MODEL_NAME = "openai/whisper-tiny"
 BATCH_SIZE = 8
 FILE_LIMIT_MB = 1000
 YT_LENGTH_LIMIT_S = 3600  # limit to 1 hour YouTube files
